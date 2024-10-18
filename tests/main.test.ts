21443e24fc631c3dded5e668a22e92c3b620e613
Sepide Kia <sepidekia6990@gmail.com>
2024-10-18 17:15:42 +0330

    feat: config mock service worker and use it in a demo file

---
+++ tests/main.test.ts
@@ -0,0 +1,8 @@
+describe("group", () => {
+  it("should", async () => {
+    const response = await fetch("./categories");
+    const data = await response.json();
+    console.log(data);
+    expect(data).toHaveLength(3);
+  });
+});
