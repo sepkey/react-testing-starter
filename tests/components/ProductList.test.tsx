30b5b55220a3ca1b5a967dae399d9cf08a5915cc
Sepide Kia <sepidekia6990@gmail.com>
2024-10-22 15:29:35 +0330

    feat: write test for ProductList

---
+++ tests/components/ProductList.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import ProductList from "../../src/components/ProductList";
+import { server } from "../mocks/server";
+import { http, HttpResponse } from "msw";
+
+describe("ProductList", () => {
+  it("should render list of products", async () => {
+    render(<ProductList />);
+
+    const items = await screen.findAllByRole("listitem");
+    expect(items.length).toBeGreaterThan(0);
+  });
+
+  it("should render no products if no product is found", async () => {
+    server.use(http.get("./products", () => HttpResponse.json([])));
+    render(<ProductList />);
+
+    const message = await screen.findByText(/no products/i);
+    expect(message).toBeInTheDocument();
+  });
+});
