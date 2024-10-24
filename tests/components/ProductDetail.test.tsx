55806c9e7a83d9dc920869108ec4ef04e3dc80ee
Sepide Kia <sepidekia6990@gmail.com>
2024-10-24 16:09:29 +0330

    feat:  use db  in product details

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,22 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
-import { products } from "../mocks/data";
 import { server } from "../mocks/server";
 import { http, HttpResponse } from "msw";
+import { db } from "../mocks/db";
 
 describe("ProductDetail", () => {
-  it("should render product", async () => {
-    render(<ProductDetail productId={1} />);
+  let productId: number;
+  beforeAll(() => {
+    const product = db.product.create();
+    productId = product.id;
+  });
+
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: productId } } });
+  });
+  it("should render product details", async () => {
+    const product = db.product.findFirst({
+      where: { id: { equals: productId } },
+    });
+    render(<ProductDetail productId={productId} />);
     expect(
-      await screen.findByText(new RegExp(products[0].name))
+      await screen.findByText(new RegExp(product!.name))
     ).toBeInTheDocument();
 
     expect(
-      await screen.findByText(new RegExp(products[0].price.toString()))
+      await screen.findByText(new RegExp(product!.price.toString()))
     ).toBeInTheDocument();
   });
 
-  it("should render no product if not found is found", async () => {
+  it("should render no product if not found ", async () => {
     server.use(http.get("/products/1", () => HttpResponse.json(null)));
     render(<ProductDetail productId={1} />);
     const message = await screen.findByText(/not found/i);
